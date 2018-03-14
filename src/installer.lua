86d351dbf8934ffc616073cb088522fcc9095226
ben-mkiv <advoztyerusdowranhivcin2@gmail.com>
2018-03-14 03:11:37 +0100

    cleaaaaaanup

---
+++ src/installer.lua
@@ -0,0 +1,24 @@
+-- generic installer
+fs = require("filesystem")
+
+files = {
+    { pastebin = "QPZZrQdS", path = "/usr/lib", filename = "borders.lua", label = "border/frame lib" },
+    { pastebin = "9Ujn0eEn", path = "/usr/lib", filename = "hazeUI.lua", label = "hazeUI lib" }
+}
+
+function fetchFile(f)
+    local file = f.path .. "/" .. f.filename
+    if not fs.isDirectory(f.path) then fs.makeDirectory(f.path); end
+    if fs.exists(file) then fs.remove(file); end
+
+    print("fetching "..f.label)
+    os.execute("pastebin get ".. f.pastebin .." " .. file)
+end
+
+
+for i=1,#files do
+    fetchFile(files[i])
+    os.sleep(0)
+end
+
+print("\n...done!")
